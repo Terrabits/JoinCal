06ec745565643e3880b386b3d06c66f23ce3a03a
Nick Lalic <nick.lalic@gmail.com>
2016-05-26 12:10:06 -0700

    Started adding address to VnaTestClass and my test classes

--- Test/main.cpp
+++ Test/main.cpp
@@ -9,6 +9,7 @@
 #include "GetCalDialogTest.h"
 
 // RsaToolbox
+#include <Definitions.h>
 #include <TestClass.h>
 #include <TestRunner.h>
 using namespace RsaToolbox;
@@ -21,13 +22,16 @@ using namespace RsaToolbox;
 
 int main()
 {
+    RsaToolbox::ConnectionType connectionType = RsaToolbox::ConnectionType::VisaTcpSocketConnection;
+    QString address = "127.0.0.1::5025";
+
     TestRunner testRunner;
     testRunner.addTest(new CalibrationSourceTest);
     testRunner.addTest(new CalibrationTest);
-    testRunner.addTest(new CorrectionsTest);
-    testRunner.addTest(new JoinCalibrationsTest);
-    testRunner.addTest(new GetCalDialogTest);
-    testRunner.addTest(new CalSortTest);
+    testRunner.addTest(new CorrectionsTest(connectionType, address));
+    testRunner.addTest(new JoinCalibrationsTest(connectionType, address));
+    testRunner.addTest(new GetCalDialogTest(connectionType, address));
+    testRunner.addTest(new CalSortTest(connectionType, address));
 
     qDebug() << "Global result: " << (testRunner.runTests() ? "PASS" : "FAIL");
     return 0;
